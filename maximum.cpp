f46ca2c9dcc180761bae98817db3cf3ff335237a
Gauri Verma <114560615+GauriVerma002@users.noreply.github.com>
2023-03-22 01:39:33 +0800

    Create maximum.cpp

---
+++ maximum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int area(int a, int b)
+{ 
+    return max(a,b);
+}
+
+int main()
+{
+    //cout << area(2, 8) << endl;
+    cout << area(-2, -8) << endl;
+}
